1fd0d17e1ccd32839212e3e5b1c2fdf461816b78
IgMehle <igmehlegtr@gmail.com>
2025-08-07 10:32:18 -0300

    Eeprom actualizada con queueset

--- 1_ega/2_workspace/firmware/firmware.c
+++ 1_ega/2_workspace/firmware/firmware.c
@@ -38,14 +38,24 @@
 
 #define MENU_SIZE       5
 
+#define EEPROM_QUEUE_SIZE   1
+#define EEPROM_DATA_BASE    0x0010
+#define EEPROM_DATA_SIZE    0x0010
+
 // Handles de tareas
 TaskHandle_t tPWM, tBH1750, tEEPROM, tLCD, tEncoder, tPulsador, tRUN;
 // Queues de datos inter tarea
 QueueHandle_t  qCTRL, qPWM, qLUX, qLCD, qEread, qEwrite;
+// Queue Set de task_EEPROM
+QueueSetHandle_t qsetEEPROM;
+// Queues de manejo de eeprom
+QueueHandle_t qEreadReq, qEcalibReq, qEdumpReq, qEclearReq;
+// Queues de datos de eeprom
+QueueHandle_t qEwrite, qEwritecalib, qEread, qEreadcalib, qElog;
 // Mutex bus I2C1
 SemaphoreHandle_t mI2C;
 // Semaforos de control
-SemaphoreHandle_t sPULS, sA, sB, sEwrite, sEread;
+SemaphoreHandle_t sPULS, sA, sB, sRefresh;
 
 // ITEM DE QUEUE LCD
 typedef struct {
@@ -54,7 +64,8 @@ typedef struct {
     uint8_t clear;
 } qLCD_t;
 
-// ESTRUCTURA BASE DE SETTINGS
+#pragma pack(push, 1)
+// ESTRUCTURA BASE DE SETTINGS (16B)
 typedef struct {
     uint16_t index; // 2B
     uint16_t lux; // 2B
@@ -64,6 +75,13 @@ typedef struct {
     rtc_t time; // 7B
 } settings_t;
 
+#pragma pack(pop)
+// BUFFER DE SETTINGS
+typedef union {
+    settings_t settings;
+    uint8_t packet[16];
+} settings_buffer_t;
+
 // ESTRUCTURA DE MENU
 typedef struct {
     const char* texto;
@@ -72,16 +90,77 @@ typedef struct {
     int max;
 } menu_t;
 
-// ----- STRINGS PARA IMPRIMIR EN LCD ----- //
-char text1[] = {"NIVEL LUX:"};
-char text2[] = {"ALARMA MAX:"};
-char text3[] = {"ALARMA MIN:"};
-char text4[] = {"AJUSTE:"};
-char text5[] = {"GRADUAL"};
-char text6[] = {"DIRECTO"};
-char text7[] = {"CALIBRACION?"};
-char text10[] = {"GUARDANDO..."};
-char text11[] = {"LUX: "};
+// ESTRUCTURA DE PARAMETROS CALIBRACION
+typedef struct {
+    uint8_t params;
+} calibration_t;
+
+// Estructura para read request
+typedef struct {
+    QueueHandle_t read_q;
+} eepromReadRequest_t;
+
+// Estructura para calibration request
+typedef struct {
+    QueueHandle_t calib_q;
+} eepromCalibRequest_t;
+
+// Estructura para dump request
+typedef struct {
+    QueueHandle_t log_q;
+} eepromDumpRequest_t;
+
+// Estructura para clear request
+typedef struct {
+    QueueHandle_t clear_q; // puede ser NULL
+} eepromClearRequest_t;
+
+// // ----- STRINGS PARA IMPRIMIR EN LCD ----- //
+// char text1[] = {"NIVEL LUX:"};
+// char text2[] = {"ALARMA MAX:"};
+// char text3[] = {"ALARMA MIN:"};
+// char text4[] = {"AJUSTE:"};
+// char text5[] = {"GRADUAL"};
+// char text6[] = {"DIRECTO"};
+// char text7[] = {"CALIBRACION?"};
+// char text10[] = {"GUARDANDO..."};
+// char text11[] = {"LUX: "};
+
+void settings2bytes(settings_t *settings, uint8_t *bytes)
+{
+    bytes[0] = (uint8_t)(((settings->index)>>8) & 0x00FF);
+    bytes[1] = (uint8_t)(settings->index & 0x00FF);
+    bytes[2] = (uint8_t)(((settings->lux)>>8) & 0x00FF);
+    bytes[3] = (uint8_t)(settings->lux & 0x00FF);
+    bytes[4] = (uint8_t)(((settings->user_max)>>8) & 0x00FF);
+    bytes[5] = (uint8_t)(settings->user_max & 0x00FF);
+    bytes[6] = (uint8_t)(((settings->user_min)>>8) & 0x00FF);
+    bytes[7] = (uint8_t)(settings->user_min & 0x00FF);
+    bytes[8] = settings->curva;
+    bytes[9] = settings->time.sec;
+    bytes[10] = settings->time.min;
+    bytes[11] = settings->time.hour;
+    bytes[12] = settings->time.weekday;
+    bytes[13] = settings->time.day;
+    bytes[14] = settings->time.month;
+    bytes[15] = settings->time.year;
+}
+
+void bytes2settings(settings_t *settings, uint8_t *bytes)
+{
+    settings->index = ((bytes[0]<<8) & 0xFF00) + bytes[1];
+    settings->lux = ((bytes[2]<<8) & 0xFF00) + bytes[3];
+    settings->user_max = ((bytes[0]<<8) & 0xFF00) + bytes[1];
+    settings->user_min = ((bytes[0]<<8) & 0xFF00) + bytes[1];
+    settings->curva = bytes[8];
+    settings->time.sec = bytes[9];
+    settings->time.min = bytes[10];
+    settings->time.hour = bytes[11];
+    settings->time.weekday = bytes[12];
+    settings->time.day = bytes[13];
+    settings->time.month = bytes[14];
+    settings->time.year = bytes[15];
+}
 
 void irq_encoder(uint gpio, uint32_t events)
 {
@@ -204,45 +283,99 @@ void task_BH1750(void *pvParams)
 
 void task_EEPROM(void *pvParams)
 {
-    rtc_t time;
-    uint8_t bf[7];
-    uint16_t address = 0x0010;
+    // request de datos
+    QueueHandle_t read_q;
+    QueueHandle_t calib_q;
+    QueueHandle_t dump_q;
+    QueueHandle_t clear_q;
+    //read_req.read_q = qEread;
+    //calib_req.calib_q = qEreadcalib,
+    //dump_req.log_q = qElog;
+    //clear_req.clear_q = qEclear;
+    // selector de queue
+    QueueSetMemberHandle_t active_queue;
+
+    settings_t settings;
+    calibration_t calib;
+    uint8_t bf[16], bf_calib[16];
+    uint16_t address = 0x0000;
+    uint16_t index = 0;
+
     while(1){
-        // Intento leer de la queue de escritura
-        if(xQueueReceive(qEwrite, &time, portMAX_DELAY) == pdPASS){
-            // Desempaqueto
-            bf[0] = time.sec;
-            bf[1] = time.min;
-            bf[2] = time.hour;
-            bf[3] = time.weekday;
-            bf[4] = time.day;
-            bf[5] = time.month;
-            bf[6] = time.year;
-            // Intento tomar el bus
-            if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
-                // Escribo hora en la eeprom
-                eeprom_write(bf, address, 9);
-                xSemaphoreGive(mI2C);
+        active_queue = xQueueSelectFromSet(qsetEEPROM, portMAX_DELAY);
+        // ESCRITURA SETTINGS
+        if (active_queue == qEwrite)
+        {
+            // Intento leer de la queue de escritura
+            if(xQueueReceive(qEwrite, &settings, 0) == pdPASS){
+                // Calculo address
+                address = index*EEPROM_DATA_SIZE + EEPROM_DATA_BASE;
+                // Desempaqueto
+                settings2bytes(&settings, &bf);
+                // Intento tomar el bus
+                if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
+                    // Escribo settings en la eeprom
+                    eeprom_write(bf, address, EEPROM_DATA_SIZE);
+                    xSemaphoreGive(mI2C);
+                }
+            }   
+        }
+        // LECTURA SETTINGS
+        else if (active_queue == qEreadReq){
+            if(xQueueReceive(qEreadReq, &read_q, 0) == pdPASS){
+                // Calculo address
+                address = index*EEPROM_DATA_SIZE + EEPROM_DATA_BASE;
+                // Intento tomar el bus
+                if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
+                    // Leo el ultimo registro de la eeprom
+                    eeprom_read(bf, address, 16);
+                    xSemaphoreGive(mI2C);
+                }
+                // Empaqueto
+                bytes2settings(&settings, &bf);
+                // Mando a la cola de lectura
+                xQueueSend(qEread, &settings, portMAX_DELAY);
+                //xQueueSend(read_q, &bf, portMAX_DELAY);
             }
         }
-        // Intento tomar el semaforo de lectura
-        if(xSemaphoreTake(sEread, portMAX_DELAY) == pdPASS){
-            // Intento tomar el bus
-            if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
-                // Leo la eeprom
-                eeprom_read(bf, address, 7);
-                xSemaphoreGive(mI2C);
+        // ESCRITURA CALIBRACION
+        if (active_queue == qEwritecalib)
+        {
+            // Intento leer de la queue de escritura de calibracion
+            if(xQueueReceive(qEwritecalib, &bf_calib, 0) == pdPASS){
+                // Intento tomar el bus
+                if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
+                    // Escribo parametros de calibracion en la eeprom
+                    eeprom_write(bf_calib, 0x0000, 16);
+                    xSemaphoreGive(mI2C);
+                }
+            }   
+        }
+        // LECTURA CALIBRACION
+        else if (active_queue == qEreadReq){
+            if(xQueueReceive(qEcalibReq, &calib_q, 0) == pdPASS) {
+                // Intento tomar el bus
+                if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
+                    // Leo parametros de calibracion de la eeprom
+                    eeprom_read(bf_calib, 0x0000, 16);
+                    xSemaphoreGive(mI2C);
+                }
+                // Mando a la cola de lectura de parametros del pid
+                xQueueSend(qEreadcalib, &bf_calib, portMAX_DELAY);
+                //xQueueSend(calib_q, &bf_calib, portMAX_DELAY);
             }
-            // Empaqueto
-            time.sec = bf[0];
-            time.min = bf[1];
-            time.hour = bf[2];
-            time.weekday = bf[3];
-            time.day = bf[4];
-            time.month = bf[5];
-            time.year = bf[6];
-            // Mando a la cola de lectura
-            xQueueSend(qEread, &time, portMAX_DELAY);
+        }
+        // LOGGER DE DATOS
+        else if (active_queue == qEdumpReq){
+            if(xQueueReceive(qEdumpReq, &dump_q, 0) == pdPASS){
+
+            }
+        }
+        // BORRADO DE DATOS
+        else if (active_queue == qEclearReq){
+            if(xQueueReceive(qEclearReq, &clear_q, 0) == pdPASS){
+                
+            }
         }
     }
 }
@@ -273,12 +406,15 @@ void task_Setear(void *params)
 {
     // respuesta de lectura de queue
     BaseType_t rx;
+    // request de lectura de datos
+    QueueHandle_t read_req = qEreadReq;
     // struct de hora
     rtc_t time = {0};
     // lineas del lcd
     qLCD_t lcd;
     // settings
     settings_t settings;
+    uint16_t num_log = 0;
     // valores del menu
     menu_t menu[MENU_SIZE] = {
         {"NIVEL LUX:", 10, 0, 100},
@@ -299,10 +435,20 @@ void task_Setear(void *params)
         rx = xQueueReceive(qCTRL, &opc, portMAX_DELAY);
         if(rx == pdPASS && opc == 'P'){
             // ENTRO EN CONFIGURACION
+
             // Levanto la config actual de la eeprom
-            // settings <- EEPROM
-            // Cargo valores al menu
-            // menu.valor <- settings
+            xQueueSend(qEreadReq, &read_req, portMAX_DELAY);
+            // Cambio de contexto para leer
+            taskYIELD();
+             // settings <- EEPROM
+            xQueueReceive(qEread, &settings, portMAX_DELAY);
+            // Cargo settings actuales al menu
+            menu[0].valor = settings.lux;
+            menu[1].valor = settings.user_max;
+            menu[2].valor = settings.user_min;
+            menu[3].valor = settings.curva;
+            menu[4].valor = 0; // No hago calibracion salvo que especifique
+            num_log = settings.index;
         
             // limpio variable
             opc = 0;
@@ -319,29 +465,24 @@ void task_Setear(void *params)
                     sprintf(lcd.text, "%s", menu[indice].texto);
                     lcd.line = 0;
                     lcd.clear = 1;
-                    ///// -> LCD
-                    // xQueue(qLCD, &lcd, 0);
                     xQueueSend(qLCD, &lcd, portMAX_DELAY);
                     taskYIELD();
                     // Muestro valor actualizado en LCD
                     sprintf(lcd.text, "%4d", menu[indice].valor);
                     lcd.line = 1;
                     lcd.clear = 0;
-                    ///// -> LCD
-                    // xQueueSend(qLCD, &lcd, 0);
-                    //xQueueOverwrite(qLCD, &lcd);
                     xQueueSend(qLCD, &lcd, portMAX_DELAY);
                     taskYIELD();
                     // limpio variable
                     show_menu = 0;
                 }
-                printf("Indice: %d, Opc: %c, Show: %d", indice, opc, show_menu);
+                //printf("Indice: %d, Opc: %c, Show: %d/n", indice, opc, show_menu);
                 // Recepcion no bloqueante (NO HACER PEEK)
                 rx = xQueueReceive(qCTRL, &opc, portMAX_DELAY);
                 // Si no llega un comando no lo proceso,
                 // Continuo con la proxima iteracion
                 //if(rx != pdPASS) continue;
-                if(rx == pdPASS) printf("Comando recibido: %c\n", opc);
+                //if(rx == pdPASS) printf("Comando recibido: %c\n", opc);
                 // Comando
                 switch (opc){
                 case 'H':
@@ -352,8 +493,6 @@ void task_Setear(void *params)
                         sprintf(lcd.text, "%4d", menu[indice].valor);
                         lcd.line = 1;
                         lcd.clear = 0;
-                        ///// -> LCD
-                        //xQueueOverwrite(qLCD, &lcd);
                         xQueueSend(qLCD, &lcd, portMAX_DELAY);
                         taskYIELD();
                     }
@@ -367,8 +506,6 @@ void task_Setear(void *params)
                         sprintf(lcd.text, "%4d", menu[indice].valor);
                         lcd.line = 1;
                         lcd.clear = 0;
-                        ///// -> LCD
-                        //xQueueOverwrite(qLCD, &lcd);
                         xQueueSend(qLCD, &lcd, portMAX_DELAY);
                         taskYIELD();
                     }
@@ -387,28 +524,46 @@ void task_Setear(void *params)
             }
             // Si calibracion == 1
             // calibracion();
+
+            // Leo la hora en el rtc
+            if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
+                rtc_read(&time);
+                xSemaphoreGive(mI2C);
+                settings.time = time;
+            }
             // GUARDANDO CONFIG ... -> LCD
             sprintf(lcd.text, "GUARDANDO");
             lcd.line = 0;
             lcd.clear = 1;
-            //xQueueOverwrite(qLCD, &lcd);
             xQueueSend(qLCD, &lcd, portMAX_DELAY);
             taskYIELD();
             sprintf(lcd.text, "CONFIGURACION");
             lcd.line = 1;
             lcd.clear = 0;
-            //xQueueOverwrite(qLCD, &lcd);
             xQueueSend(qLCD, &lcd, portMAX_DELAY);
             taskYIELD();
+
             // DELAY 1SEG
-            // MENU -> settings
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            // cargo menu actualizado a settings
+            settings.lux = menu[0].valor;
+            settings.user_max = menu[1].valor;
+            settings.user_min = menu[2].valor;
+            settings.user_max = menu[3].valor;
+            settings.curva = menu[4].valor;
             // settings -> EEPROM
+            xQueueSend(qEwrite, &settings, portMAX_DELAY);
+            taskYIELD();
+            // Doy la orden a task_Control parta que refresque los datos
+            xSemaphoreGive(sRefresh);
         }
     }   
 }
 
 void task_Control(void *pvParams)
 {
+    settings_t settings;
+    calibration_t calib;
     uint16_t lux;
     float delta;
     qLCD_t lcd;
@@ -416,36 +571,36 @@ void task_Control(void *pvParams)
     uint8_t bf[7];
 
     while(1){
+        // Actualizar seteos ? (NO BLOQUEANTE)
+        if(xSemaphoreTake(sRefresh, 0)){
+            // Levanto valores de la eeprom
+        }
         // Hay lectura del luxometro ?
         if(xQueueReceive(qLUX, &lux, portMAX_DELAY) == pdPASS){
             /////////////////
             // CONTROL PWM //
             /////////////////
             xQueueSend(qPWM, &delta, portMAX_DELAY);
+
             // Armo el texto para el LCD
             sprintf(lcd.text, "LUX: %d", lux);
             lcd.line = 0;
+            lcd.clear = 1;
             // Envio a la cola del lcd
             xQueueSend(qLCD, &lcd, portMAX_DELAY);
             // Fuerzo cambio de contexto
             taskYIELD();
-            // Leo ultima hora en eeprom
-            xSemaphoreGive(sEread);
-            taskYIELD();
-            // leo queue de eeprom
-            xQueueReceive(qEread, &time, portMAX_DELAY);
-            // Desempaqueto
-            bf[0] = time.sec;
-            bf[1] = time.min;
-            bf[2] = time.hour;
-            bf[3] = time.weekday;
-            bf[4] = time.day;
-            bf[5] = time.month;
-            bf[6] = time.year;
+
+            // Leo la hora en el rtc
+            if(xSemaphoreTake(mI2C, portMAX_DELAY) == pdPASS){
+                rtc_read(&time);
+                xSemaphoreGive(mI2C);
+            }
             // muestro la ultima hora
             sprintf(lcd.text, "%02d/%02d %02d:%02d:%02d", time.day, time.month, 
                                 time.hour, time.min, time.sec);
             lcd.line = 1;
+            lcd.clear = 0;
             // Escribo hora en el LCD
             xQueueSend(qLCD, &lcd, portMAX_DELAY);
         }
@@ -555,31 +710,52 @@ int main()
     xSemaphoreGive(sA);
     vSemaphoreCreateBinary(sB);
     xSemaphoreGive(sB);
-    vSemaphoreCreateBinary(sEwrite);
-    xSemaphoreGive(sEwrite);
-    vSemaphoreCreateBinary(sEread);
-    xSemaphoreGive(sEread);
+    vSemaphoreCreateBinary(sRefresh);
     
     // Creo mutex y lo libero
     mI2C = xSemaphoreCreateMutex();
     xSemaphoreGive(mI2C);
 
-    // Creo queues
+    // Creo queues de programa
     qCTRL = xQueueCreate(5, sizeof(char));
     qLCD = xQueueCreate(1, sizeof(qLCD_t));
     qLUX = xQueueCreate(1, sizeof(uint16_t));
     qPWM = xQueueCreate(1, sizeof(float));
-    qEread = xQueueCreate(1, sizeof(rtc_t));
-    qEwrite = xQueueCreate(1, sizeof(rtc_t));
+    // Creo queues "semaforo" de eeprom
+    qEreadReq = xQueueCreate(1, sizeof(QueueHandle_t));
+    qEcalibReq = xQueueCreate(1, sizeof(QueueHandle_t));
+    qEdumpReq = xQueueCreate(1, sizeof(QueueHandle_t));
+    qEclearReq = xQueueCreate(1, sizeof(QueueHandle_t));
+    // Creo queues de datos de eeprom
+    qEwrite = xQueueCreate(1, sizeof(settings_t));
+    qEwritecalib = xQueueCreate(1, sizeof(calibration_t));
+    qEread = xQueueCreate(1, sizeof(settings_t));
+    qEreadcalib = xQueueCreate(1, sizeof(calibration_t));
+    qElog = xQueueCreate(1, sizeof(char)); 
+
+    // Queues de manejo de eeprom
+//QueueHandle_t qEreadReq, qEcalibReq, qEdumpReq, qEclearReq;
+// Queues de datos de eeprom
+//QueueHandle_t qEwrite, qEcalib, qEread, qEreadCalib;
+
+    // Creo queue set de eeprom
+    qsetEEPROM = xQueueCreateSet(EEPROM_QUEUE_SIZE*6);
+    // Agrego las colas al set
+    xQueueAddToSet(qEreadReq, qsetEEPROM);
+    xQueueAddToSet(qEcalibReq, qsetEEPROM);
+    xQueueAddToSet(qEdumpReq, qsetEEPROM);
+    xQueueAddToSet(qEclearReq, qsetEEPROM);
+    xQueueAddToSet(qEwrite, qsetEEPROM);
+    xQueueAddToSet(qEwritecalib, qsetEEPROM);
 
     // Creo tareas
     // xTaskCreate(task_Control, "CONTROL", 2*configMINIMAL_STACK_SIZE, NULL, 1, NULL);
     // xTaskCreate(task_BH1750, "BH1750", configMINIMAL_STACK_SIZE, NULL, 2, &tBH1750);
     // xTaskCreate(task_PWM, "PWM", configMINIMAL_STACK_SIZE, NULL, 2, &tPWM);
-    xTaskCreate(task_LCD, "LCD", configMINIMAL_STACK_SIZE, NULL, 3, &tLCD);
-    // xTaskCreate(task_EEPROM, "EEPROM", configMINIMAL_STACK_SIZE, NULL, 3, &tEEPROM);
+    xTaskCreate(task_LCD, "LCD", 128, NULL, 3, &tLCD);
+    xTaskCreate(task_EEPROM, "EEPROM", 256, NULL, 3, &tEEPROM);
     xTaskCreate(task_Setear, "SETEAR", 512, NULL, 2, &tPulsador);
-    xTaskCreate(task_LedRun, "LEDRUN", configMINIMAL_STACK_SIZE, NULL, 2, &tRUN);
+    xTaskCreate(task_LedRun, "LEDRUN", 128, NULL, 2, &tRUN);
     xTaskCreate(task_Pulsador, "Pulsador", 128, NULL, 4, NULL);
     xTaskCreate(task_EncoderA, "EncoderA", 128, NULL, 4, NULL);
     xTaskCreate(task_EncoderB, "EncoderB", 128, NULL, 4, NULL);
