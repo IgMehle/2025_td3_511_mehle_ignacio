97f702d399eea938033478a6985afad90166ecb0
IgMehle <igmehlegtr@gmail.com>
2025-10-10 19:03:50 -0300

    Primer consigna de TP5

--- 3_trabajos_practicos/tp5/kernel_module.c
+++ 3_trabajos_practicos/tp5/kernel_module.c
@@ -1,15 +1,79 @@
 #include <linux/module.h>
 #include <linux/init.h>
-#include "gpio_driver.h"
+#include <linux/kernel.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+//#include "gpio_driver.h"
 
 // Etiqueta para el autor del modulo
-#define AUTHOR	""
+#define AUTHOR "IgMehle"
 
-/**
- * @brief Se llama cuando el modulo se carga en el kernel
-*/
+// Puntero para primer hilo
+static struct task_struct *thread1;
+// Puntero para segundo hilo
+static struct task_struct *thread2;
+
+
+/// @brief Hilo 1 periodico en kernel
+/// @param params Puntero a parametros
+/// @return 0 si OK
+static int thread1_f(void *params) {
+    // Corre mientras no haya otros procesos que lo detengan
+    while(!kthread_should_stop()) {
+        // Mensaje para el Kernel
+        printk(KERN_INFO "%s: Hola desde el kernel!\n", AUTHOR);
+        // Demora de un segundo
+        msleep(1000);
+    }
+    return 0;
+}
+
+/// @brief Hilo 2 periodico en kernel
+/// @param params Puntero a parametros
+/// @return 0 si OK
+static int thread2_f(void *params) {
+    // Corre mientras no haya otros procesos que lo detengan
+    while(!kthread_should_stop()) {
+		// Demora de medio segundo
+		msleep(500);
+        // Mensaje para el Kernel
+        printk(KERN_INFO "%s: Chau desde el kernel!\n", AUTHOR);
+        // Demora de medio segundo
+		msleep(500);
+    }
+    return 0;
+}
+
+
+/// @brief Callback que inicializa el modulo en el kernel
+/// @return 0 si OK
 static int __init kernel_module_init(void) {
-	// TODO
+	// Mensaje para el kernel
+	printk(KERN_INFO "%s: Insertando el modulo de kernel\n", AUTHOR);
+    // Intento crear y correr el hilo 1
+	thread1 = kthread_run(
+        thread1_f,  // Callback
+        NULL,       // Sin datos
+        "thread1"   // Nombre del hilo
+    );
+    // Verifico si hubo error al crearlo
+    if (IS_ERR(thread1)) {
+        printk(KERN_ERR "%s: Error al crear thread 1\n", AUTHOR);
+        return -1;
+    }
+    // Intento crear y correr el hilo 2
+    thread2 = kthread_run(
+        thread2_f,  // Callback
+        NULL,       // Sin datos
+        "thread2"   // Nombre del hilo
+    );
+    // Verifico si hubo error al crearlo
+    if (IS_ERR(thread2)) {
+        printk(KERN_ERR "%s: Error al crear thread 2\n", AUTHOR);
+        // Elimino el hilo anterior
+        kthread_stop(thread1);
+        return -1;
+    }
 	return 0;
 }
 
@@ -17,7 +81,18 @@ static int __init kernel_module_init(void) {
  * @brief Se llama cuando el modulo se quita del kernel
  */
 static void __exit kernel_module_exit(void) {
-	// TODO
+	// Mensaje para el Kernel
+	pr_info("%s: Removiendo el modulo de kernel\n", AUTHOR);
+    // Si se habia podido crear el hilo
+	if (thread1) {
+        // Detengo el hilo
+        kthread_stop(thread1);
+    }
+    // Si se habia podido crear el hilo
+    if (thread2) {
+        // Detengo el hilo
+        kthread_stop(thread2);
+    }
 }
 
 // Registro la funcion de inicializacion y salida
